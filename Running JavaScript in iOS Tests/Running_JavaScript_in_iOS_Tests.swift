f93ca5beec6791c82117b3fbe603a2f3d1ddcfbb
Samantha Gatt <smfgatt@gmail.com>
2020-02-17 13:31:48 -0500

    Starts unit testing

--- Running JavaScript in iOS Tests/Running_JavaScript_in_iOS_Tests.swift
+++ Running JavaScript in iOS Tests/Running_JavaScript_in_iOS_Tests.swift
@@ -9,5 +9,25 @@
 import XCTest
 
 class Running_JavaScript_in_iOS_Tests: XCTestCase {
+    
+    func testJumboOperation() {
+        let testId = "test1"
+        let testProg: Float = 0.5
+        let testSucceed: Bool? = nil
+        let testIndex = 0
+        let testOp = JumboOperation(id: testId, progress: testProg, succeeded: testSucceed, index: testIndex)
+        
+        XCTAssertEqual(testId, testOp.id)
+        XCTAssertEqual(testProg, testOp.progress)
+        XCTAssertEqual(testSucceed, testOp.succeeded)
+        XCTAssertEqual(testIndex, testOp.index)
+    }
 
+    func testAddOperationReturnsCorrectId() {
+        let testOp = JumboOperation(id: "test1", index: 0)
+        let testOpController = JumboOperationController()
+        
+        let opId = testOpController.addOperation(testOp.id, index: testOp.index)
+        XCTAssertEqual(testOp.id, opId)
+    }
 }
